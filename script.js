9318f8da3442e238cad370006e9e99946a72fc5c
cma-khatibi <68824061+cma-khatibi@users.noreply.github.com>
2020-07-26 23:39:49 +0430

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 const generateTarget = () => Math.floor(Math.random() * 9);
+
 const compareGuesses = (userGuess, compGuess, target) => {
+  if (userGuess <! 10 || userGuess >! -1) {
+    alert('guess number between 0-9');
+    return false;
+  }
   userDistance = Math.abs(target - userGuess);
   compDistance = Math.abs(target - compGuess);
   if (userDistance > compDistance) {
@@ -13,9 +18,9 @@ const compareGuesses = (userGuess, compGuess, target) => {
   }
 };
 
+
 const updateScore = winner => {
   winner === 'human' ? humanScore += 1 : computerScore += 1;
 };
 
 const advanceRound = () => currentRoundNumber += 1;
-
