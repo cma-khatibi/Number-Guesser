67622a7dff8ae944ec4f4d38499413dd19effcfd
cma-khatibi <68824061+cma-khatibi@users.noreply.github.com>
2020-07-26 23:23:38 +0430

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 const generateTarget = () => Math.floor(Math.random() * 9);
-// console.log(generateTarget())
 const compareGuesses = (userGuess, compGuess, target) => {
   userDistance = Math.abs(target - userGuess);
   compDistance = Math.abs(target - compGuess);
